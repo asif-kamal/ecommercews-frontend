2e7c2684299aa31ffa45f13f49070b1119c2cbfd
asif-kamal <asif.kamal1489@gmail.com>
2024-11-04 13:27:22 -0500

    added tailwind css defaults and configuration

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // Make sure this includes your component path
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
