b5aa5dd901c0e98e7464ddf557a2c16105e140a8
asif-kamal <asif.kamal1489@gmail.com>
2024-11-04 14:54:50 -0500

    used routing to create paths for elements

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import Navbar from './components/layout/NavBar';
-import Footer from './components/layout/Footer';
 import ElectronicsDisplay from './components/electronics/ElectronicsDisplay';
-import content from './data/content.json'
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import CartItems from './components/cart/CartItems';  // Create if not exists
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-50">
-      <Navbar />
-      <main className="flex-grow">
-        <ElectronicsDisplay />
-      </main>
-      <Footer content={content?.footer}/>
-    </div>
+    <Routes>
+      <Route path="/api/electronics" element={<Layout />}>
+        <Route index element={<ElectronicsDisplay />} />
+        <Route path="computers" element={<ElectronicsDisplay />} />
+        <Route path="tv" element={<ElectronicsDisplay />} />
+        <Route path="audiovideo" element={<ElectronicsDisplay />} />
+        <Route path="shop" element={<ElectronicsDisplay />} />
+        <Route path="cart-items" element={<CartItems />} />
+      </Route>
+    </Routes>
   );
 }
 
