9b322e0db81ec8e1a1e6b3cdca9b223c9ded5d95
asif-kamal <asif.kamal1489@gmail.com>
2024-11-04 20:04:16 -0500

    Updated Layout so HeroSection only appears when you navigate to the landing page

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,12 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./NavBar";
 import Footer from "./Footer";
 import content from "../../data/content.json";
+import HeroSection from "./HeroSection";
 
 const Layout = () => {
+  const location = useLocation();
+  
+  // Check if there's a page parameter in the URL
+  const searchParams = new URLSearchParams(location.search);
+  const currentPage = searchParams.get('page');
+  
+  // Show hero only on home page without pagination
+  const shouldShowHero = location.pathname === "/" && !currentPage;
+
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
+      {shouldShowHero && <HeroSection />}
       <main className="flex-grow">
         <Outlet />
       </main>
