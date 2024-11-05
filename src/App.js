d55655330016f57fcd5d5f55676f6180e43ef687
asif-kamal <asif.kamal1489@gmail.com>
2024-11-04 20:00:39 -0500

    removed unnecessary routes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import ElectronicsDisplay from './components/electronics/ElectronicsDisplay';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import CartItems from './components/cart/CartItems';  // Create if not exists
+import SearchResults from './components/search/SearchResults';  // Import SearchResults component
+import ShopDisplay from './components/shop/ShopDisplay';
 
 function App() {
   return (
     <Routes>
-      <Route path="/api/electronics" element={<Layout />}>
+      <Route path="/" element={<Layout />}>
         <Route index element={<ElectronicsDisplay />} />
-        <Route path="computers" element={<ElectronicsDisplay />} />
-        <Route path="tv" element={<ElectronicsDisplay />} />
-        <Route path="audiovideo" element={<ElectronicsDisplay />} />
-        <Route path="shop" element={<ElectronicsDisplay />} />
+        <Route path="search" element={<SearchResults />} />
+        <Route path="shop" element={<ShopDisplay />} />
         <Route path="cart-items" element={<CartItems />} />
       </Route>
     </Routes>
