894ef9bdc70efeb730ae81fca4bb6ea31fdf202f
asif-kamal <asif.kamal1489@gmail.com>
2025-09-19 19:20:28 -0500

    createing Carousel component

---
+++ src/components/layout/Carousel.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Carousel = () => {
+  return (
+    <div className="flex flex-wrap justify-around">
+      <div></div>
+      <p>Heading</p>
+    </div>
+  );
+};
+
+export default Carousel;
